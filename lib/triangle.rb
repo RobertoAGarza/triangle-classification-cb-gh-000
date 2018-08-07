1063eee00593b051a5ca6491712f354f99e39934
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 19:04:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene 
+  
+  if !valid?
+    begin 
+      raise TriangleError
+    rescue TriangleError => error 
+      puts error.message 
+    end 
+  end
+  
+  class TriangleError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+
+  def valid?
+    if @equilateral + @isosceles > @scalene and  @equilateral + @scalene > @isosceles and @isosceles + @scalene > @equilateral and @isosceles > 0 and @equilateral > 0 and @scalene > 0 
+      true 
+    else
+      false 
+    end 
+  end 
+    
+    
 end
