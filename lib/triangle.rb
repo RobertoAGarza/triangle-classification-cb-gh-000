ce3fd3c0a087d9fdd8d29592a28837f8a6b18622
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 19:24:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
+  attr_reader :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+
+  def kind
+    #test here
+    if @a == @b and @a == @c
+      :equilateral 
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+      
+  end 
 
 end
