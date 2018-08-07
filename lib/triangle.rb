83119da14ebe58ea73482991ceb6d7753f0f5b3a
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 19:35:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   end
 
   def triangleChecker
-    if @a <= 0 || @b <= 0 || @c <= 0 || @a + @b < @c || @a + @c < @b || @b + @c < @a
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
       raise TriangleError
     end
   end
