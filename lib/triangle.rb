4f263aa874bd213d62bc5d73ecf377237890ba77
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 19:24:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,22 +2,21 @@ class Triangle
   attr_reader :a, :b, :c
 
   def initialize(a,b,c)
-    @a = a 
-    @b = b 
-    @c = c 
-  end 
+    @a = a
+    @b = b
+    @c = c
+  end
 
   def kind
     #test here
     if @a == @b and @a == @c
-      :equilateral 
+      :equilateral
     elsif @a == @b || @a == @c || @b == @c
       :isosceles
     else
       :scalene
     end
   end
-      
-  end 
+
 
 end
